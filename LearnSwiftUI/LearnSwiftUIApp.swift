e6afbda7c79d842f9c3fe5e1ae1a78b24b2c1731
wander <1194409532@qq.com>
2021-10-18 08:37:55 +0800

    Initial Commit

---
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by Wander on 2021/10/18.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
