a7baa0f2d267b4da7e895c1fa57ccab77f3d6a43
wander <1194409532@qq.com>
2021-10-19 12:30:54 +0800

    创建自定义圆形图片视图

---
+++ LearnSwiftUI/UI/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  LearnSwiftUI
+//
+//  Created by Wander on 2021/10/19.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white,lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
